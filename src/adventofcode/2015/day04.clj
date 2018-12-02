0cfd9826cfa7e3cc6dcfc4f49b12ead8409c87a7
Misha Akovantsev <mikhail.akovantsev@gmail.com>
2018-12-02 21:43:41 +0200

    2015 04

---
+++ src/adventofcode/2015/day04.clj
@@ -0,0 +1,40 @@
+(ns adventofcode.2015.day04
+  (:require [clojure.string :as str])
+  (:import [java.security MessageDigest]))
+
+
+(defn md5
+  "https://gist.github.com/jizhang/4325757#gistcomment-1993162"
+  [^String s]
+  (->> s
+    .getBytes
+    (.digest (MessageDigest/getInstance "MD5"))
+    (BigInteger. 1)
+    (format "%032x")))
+
+
+(defn -f [secret n-of-zeroes]
+  (let [yes (str/join (repeat n-of-zeroes "0"))
+        no  (str yes "0")]
+    (loop [x 1]
+      (when (< x Integer/MAX_VALUE)
+        (let [cs (md5 (str secret x))]
+          (if (and
+                (-> cs (str/starts-with? yes))
+                (-> cs (str/starts-with? no) not))
+            x
+            (recur (inc x))))))))
+
+(defn f1 [secret] (-f secret 5))
+(defn f2 [secret] (-f secret 6))
+
+
+(assert (= 609043 (f1 "abcdef")))
+(assert (= 1048970 (f1 "pqrstuv")))
+
+
+
+(def input "yzbqklnj")
+
+(assert (= (f1 input) 282749))
+(assert (= (f2 input) 9962624))
