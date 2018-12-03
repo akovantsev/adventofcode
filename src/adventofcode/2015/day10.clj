d464d0e952399bb8fc62a7467ea6e2250ad3aa2b
Misha Akovantsev <mikhail.akovantsev@gmail.com>
2018-12-03 16:02:08 +0200

    2015 10 1

---
+++ src/adventofcode/2015/day10.clj
@@ -0,0 +1,26 @@
+(ns adventofcode.2015.day10)
+
+(def input "3113322113")
+
+
+(defn xform [s]
+  (->> s
+    (partition-by identity)
+    (map #(str (count %) (first %)))
+    (apply str)))
+
+(assert (= (xform "1") "11"))
+(assert (= (xform "11") "21"))
+(assert (= (xform "21") "1211"))
+(assert (= (xform "1211") "111221"))
+(assert (= (xform "111221") "312211"))
+
+(defn f1 [s]
+  (->> s
+    (iterate xform)
+    (take 41)
+    (last)
+    (count)))
+
+(assert (not= (f1 input) 252594))
+(assert (= (f1 input) 329356))
