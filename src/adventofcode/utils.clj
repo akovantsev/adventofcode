712af7898e521d6432e36492f5defe4bb274cac5
Misha Akovantsev <mikhail.akovantsev@gmail.com>
2018-12-08 18:05:24 +0200

    2018 8

---
+++ src/adventofcode/utils.clj
@@ -0,0 +1,7 @@
+(ns adventofcode.utils)
+
+
+(defmacro locals-map
+  ;; https://gist.github.com/noisesmith/3490f2d3ed98e294e033b002bc2de178
+  []
+  (into {} (for [[sym val] &env] [(keyword (name sym)) sym])))
