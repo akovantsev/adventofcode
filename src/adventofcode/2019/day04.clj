57d4571ccb698af6a47088f7a46c5fca94dca63a
Misha Akovantsev <mikhailakovantsev@gmail.com>
2019-12-04 18:20:55 +0200

    faster! 19 04

--- src/adventofcode/2019/day04.clj
+++ src/adventofcode/2019/day04.clj
@@ -13,5 +13,31 @@
 (defn f1 [passwords] (->> passwords (filter ascending?) (filter has-xwins?) count))
 (defn f2 [passwords] (->> passwords (filter ascending?) (filter has-twins?) count))
 
-(assert (= 460 (f1 PASSWORDS)))
-(assert (= 290 (f2 PASSWORDS)))
+(assert (= 460 (time (f1 PASSWORDS))))
+(assert (= 290 (time (f2 PASSWORDS))))
+"Elapsed time: 140.608874 msecs"
+"Elapsed time: 159.905614 msecs"
+
+
+
+(time ;;"Elapsed time: 14.843969 msecs" Kappa
+  (let [MIN      382345
+        MAX      843167
+        int-vec  (fn [n] (->> n str (map str) (mapv #(Integer/parseInt % 10))))
+        low      (int-vec MIN)
+        high     (int-vec MAX)
+        too-low  #(neg? (compare % low))
+        in-range #(neg? (compare % high))
+        pwds     (for [a (range 0 10)
+                       b (range a 10)
+                       c (range b 10)
+                       d (range c 10)
+                       e (range d 10)
+                       f (range e 10)
+                       :let [n [a b c d e f]]
+                       :when (->> n frequencies vals (some #{2}))]
+                   n)]
+       (->> pwds
+         (drop-while too-low)
+         (take-while in-range)
+         (count))))
