743a449157975f58d04b6cb384a680c2dbc74774
Misha Akovantsev <mikhailakovantsev@gmail.com>
2020-12-28 12:05:29 +0200

    2016 19

--- src/adventofcode/2016/day19.clj
+++ src/adventofcode/2016/day19.clj
@@ -1,40 +1,37 @@
-(ns adventofcode.2016.day19)
+(ns adventofcode.2016.day19
+  (:import [clojure.lang PersistentQueue]))
 
 (set! *print-length* 25)
 (def N 3005290)
 
 
-(defn round [elves]
-  (->> elves
-    (sequence
-      (comp
-        (map-indexed vector)
-        (remove #(-> % first odd?))
-        (map second)
-        (map-indexed vector)
-        (remove #(-> % first even?))
-        (map second)))))
+(defn p1 [input]
+  (loop [q (into PersistentQueue/EMPTY (range 1 (inc input)))]
+    (let [x (peek q) q (pop q)
+          y (peek q) q (pop q)]
+      (if (nil? y)
+        x
+        (recur (conj q x))))))
+
+(assert (= (p1 5) 3))
+(assert (= (p1 N) 1816277))
 
 
-(->> 3005290
-  (iterate #(- % (quot % 2)))
-  (take-while pos?))
-
-
-(not= 2796203)
-
-2796203
-
-(prn
-  (time
-    (let [limit 3005290]
-      (loop [[x & todo] (cycle (range 1 (inc limit)))
-             stays?     (cycle [true false])
-             banned?    (transient #{})
-             togo       (dec 3005290)]
-        ;(prn togo)
+(defn p2 [input]
+  (let [half (-> input (/ 2) Math/floor int)]
+    (loop [q1 (into PersistentQueue/EMPTY (range 1 (inc half)))
+           q2 (into PersistentQueue/EMPTY (range (inc half) (inc input)))]
+      ;(prn (mapv vec [q1 q2]))
+      (let [x (peek q1) q1 (pop q1)
+            y (peek q2) q2 (pop q2)]
+            ;z (peek q2) q2 (pop q2)]
         (cond
-          (banned? x)    (recur todo stays? banned? togo)
-          (zero? togo)   x
-          (first stays?) (recur todo (rest stays?) banned? togo)
-          :else          (recur todo (rest stays?) (conj! banned? x) (dec togo)))))))
+          (nil? x) y
+          (nil? y) x
+          :else    (if (<= (count q2) (count q1))
+                     (recur q1 (conj q2 x))
+                     (let [z (peek q2) q2 (pop q2)]
+                       (recur (conj q1 z) (conj q2 x)))))))))
+
+(assert (= (p2 5) 2))
+(assert (= (p2 N) 1410967))
